1c1e2fb3fd445c82e6452208af2c89f4a67f3654
hanvgd <hanvugiadinh07@gmail.com>
2024-02-23 13:28:41 +0700

    pingpong, find

---
+++ user/pingpong.c
@@ -0,0 +1,34 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int p[2];
+  char recv_buf[128];
+
+  pipe(p);
+
+  if(fork() == 0){
+    read(p[0], recv_buf, 4);
+    close(p[0]);
+
+    printf("%d: received %s\n", getpid(), recv_buf);
+
+    write(p[1], "pong", 4);
+    close(p[1]);
+
+    exit(0);
+  } else{
+    write(p[1], "ping", 4);
+    close(p[1]);
+
+    read(p[0], recv_buf, 4);
+    close(p[0]);
+
+    printf("%d: received %s\n", getpid(), recv_buf);
+
+    exit(0);
+  }
+}
